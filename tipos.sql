b1b25021329832e13e5c9f63a6fb24c7939e0426
Alberto Gimenez <albergimenez@gmail.com>
2010-12-07 19:47:37 -0300

    Agregado de Tipos y Titulo

---
+++ tipos.sql
@@ -0,0 +1,6 @@
+INSERT INTO tipo_imagen  ('F','FOTOS');
+INSERT INTO tipo_imagen  ('P','PLANOS');
+INSERT INTO titulo_encuestador ('N','NN','Ninguno');
+INSERT INTO titulo_encuestador ('A','Arq.','Arquitecto');
+INSERT INTO titulo_encuestador ('I','Ing.','Ingeniero');
+INSERT INTO titulo_encuestador ('L','Lic.','Licenciado');
